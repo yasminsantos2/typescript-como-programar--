62d6669603aafcb330ca2d19316802cc8f0ee6d2
yasminsantos2 <yasminsantos.eng@gmail.com>
2026-07-10 18:26:34 -0300

    feat: exibindo uma unica linha de texto com multiplas instruçoes

---
+++ welcome2.js
@@ -0,0 +1,4 @@
+// Exibindo uma linha de texto com múltiplas instruções.
+process.stdout.write("Welcome to ");
+console.log("Java Programming!");
+export {};
---
+++ welcome2.ts
@@ -0,0 +1,6 @@
+// Exibindo uma linha de texto com múltiplas instruções.
+
+process.stdout.write("Welcome to ");
+console.log("Java Programming!");
+
+export {};
